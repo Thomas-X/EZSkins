afc9f6b6e26eaf14e79643b2a149a870eb410cb7
barry-willems <Ae6kF9Pw>
2016-11-09 08:54:18 +0100

    edits

---
+++ js/button1.js
@@ -0,0 +1,66 @@
+/**
+ * Created by user on 9-11-2016.
+ */
+
+function redButton() {
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+    alert("Webshop isn't ready yet! Try again later :-)");
+}
